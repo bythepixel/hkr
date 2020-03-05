82bb7153dedeb9bcf9b55149b500a3d672612081
Carla Bobak <carla@bythepixel.com>
2020-03-05 11:30:04 -0700

    Style vote/favorite section

--- resources/js/components/IdeaVote.vue
+++ resources/js/components/IdeaVote.vue
@@ -1,18 +1,37 @@
 <template>
     <div class="vote-favorite">
-        <div class="vote" :class="{'voted': hasUserVoted(idea.votes)}">
-            <div class="vote__count">{{ idea.votes.length }} vote<span v-if="idea.votes.length !== 1">s</span></div>
-            <div class="vote__link link" @click="handleVote(idea)">Vote</div>
+        <div class="vote-favorite__count-wrapper">
+            <div class="vote-favorite__count">
+                {{ idea.votes.length }} like<span v-if="idea.votes.length !== 1">s</span>
+            </div>
+            <div class="vote-favorite__count">
+                {{ idea.favorites.length }} fave<span v-if="idea.favorites.length !== 1">s</span>
+            </div>
         </div>
-        <div class="favorite"
-            :class="{'favorited': hasUserFavorited(idea.favorites)}"
-            v-on:click="handleFavorite(idea)"
-        >
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
-                <path d="M15.668 8.626l8.332 1.159-6.065 5.874 1.48 8.341-7.416-3.997-7.416 3.997 1.481-8.341-6.064-5.874 8.331-1.159 3.668-7.626 3.669 7.626zm-6.67.925l-6.818.948 4.963 4.807-1.212 6.825 6.068-3.271 6.069 3.271-1.212-6.826 4.964-4.806-6.819-.948-3.002-6.241-3.001 6.241z"/>
-            </svg>
-            <div class="favorite__count">{{ idea.favorites.length }}</div>
+        <div class="vote-favorite__link-wrapper">
+            <div
+                class="vote-favorite__link link"
+                :class="{'interacted': hasUserVoted(idea.votes)}"
+                @click="handleVote(idea)"
+            >Like</div>
+            <div
+                class="vote-favorite__link link"
+                :class="{'interacted': hasUserFavorited(idea.favorites)}"
+                @click="handleFavorite(idea)"
+            >Fave</div>
         </div>
+
+
+
+
+<!--        <div class="vote" :class="{'voted': hasUserVoted(idea.votes)}">-->
+<!--            <div class="vote__count">{{ idea.votes.length }} like<span v-if="idea.votes.length !== 1">s</span></div>-->
+<!--            <div class="vote__link link" @click="handleVote(idea)">Like</div>-->
+<!--        </div>-->
+<!--        <div class="favorite" :class="{'favorited': hasUserFavorited(idea.favorites)}">-->
+<!--            <div class="favorite__count">{{ idea.favorites.length }} fave<span v-if="idea.favorites.length !== 1">s</span></div>-->
+<!--            <div class="favorite__link link" @click="handleFavorite(idea)">Fave</div>-->
+<!--        </div>-->
     </div>
 </template>
 
